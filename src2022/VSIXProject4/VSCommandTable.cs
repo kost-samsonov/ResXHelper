7b590df639bd2ba4014ab688cf5ea581251f43e9
Konstantin Samsonov <KNSamsonov@niitfa.ru>
2024-11-28 16:27:19 +0300

    Work project node, folder node and Item node

--- src2022/VSIXProject4/VSCommandTable.cs
+++ src2022/VSIXProject4/VSCommandTable.cs
@@ -12,18 +12,25 @@ namespace VSIXProject4
     /// </summary>
     internal sealed partial class PackageGuids
     {
-        public const string commandIconString = "4537d792-6e3d-4118-bc58-83177cc105ab";
-        public static Guid commandIcon = new Guid(commandIconString);
-
         public const string VSIXProject4String = "74863b99-dc65-4f66-b21c-9fe4bf1aaa40";
         public static Guid VSIXProject4 = new Guid(VSIXProject4String);
+
+        public const string guidGlobalPluginCmdSetString = "fa42213a-5f94-4519-90dd-38e7795e70ac";
+        public static Guid guidGlobalPluginCmdSet = new Guid(guidGlobalPluginCmdSetString);
+
+        public const string commandIconString = "4537d792-6e3d-4118-bc58-83177cc105ab";
+        public static Guid commandIcon = new Guid(commandIconString);
     }
     /// <summary>
     /// Helper class that encapsulates all CommandIDs uses across VS Package.
     /// </summary>
     internal sealed partial class PackageIds
     {
-        public const int commandIcon1 = 0x0001;
         public const int AddResourcesCommand = 0x0100;
+        public const int ProjectAddGroup = 0x1101;
+        public const int ProjectNodeGroup = 0x1102;
+        public const int FolderNodeGroup = 0x1103;
+        public const int ItemNodeGroup = 0x1104;
+        public const int commandIcon1 = 0x0001;
     }
 }
